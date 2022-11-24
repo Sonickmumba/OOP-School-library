bc9eacfffe9784dbb6f07aeef23744f7b6d5898d
sonickmumba <kingellie.mumba@gmail.com>
2022-11-24 10:36:24 +0200

    Added captialize_decorator.rb to person.rb file

--- test.rb
+++ test.rb
@@ -4,8 +4,8 @@ require './person'
 
 person = Person.new(22, 'maximilianus')
 p person.correct_name
-person.can_use_services?
-capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-capitalized_trimmed_person.correct_name
+p person.can_use_services?
+p capitalized_person = CapitalizeDecorator.new(person)
+p capitalized_person.correct_name
+p capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+p capitalized_trimmed_person.correct_name
