9a165e9f55c9998aabef63db80da195b78fea3ad
sonickmumba <kingellie.mumba@gmail.com>
2022-11-24 10:17:32 +0200

    Add the attribute accessor for the nameable

--- test.rb
+++ test.rb
@@ -2,8 +2,8 @@ require './capitalize_decorator'
 require './trimmer_decorator'
 require './person'
 
-person = Person.new(22, name: 'maximilianus')
-person.correct_name
+person = Person.new(22, 'maximilianus')
+p person.correct_name
 person.can_use_services?
 capitalized_person = CapitalizeDecorator.new(person)
 capitalized_person.correct_name
