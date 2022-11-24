2254f750f0ec76058c216c0be60f30cd5705b1a9
sonickmumba <kingellie.mumba@gmail.com>
2022-11-24 09:07:10 +0200

    Fix linter errors

--- test.rb
+++ test.rb
@@ -1,13 +1,11 @@
-require './person'
 require './capitalize_decorator'
 require './trimmer_decorator'
+require './person'
 
 person = Person.new(22, name: 'maximilianus')
-puts person.correct_name
-puts person.can_use_services?
+person.correct_name
+person.can_use_services?
 capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
+capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
-
- # puts person.correct_name
+capitalized_trimmed_person.correct_name
